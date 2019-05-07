8ca8a26aec6b5af7d3643b8a83bb88f94e99a789
Piotr <piotr@piotr.tarasinski>
2019-05-07 10:15:04 +0200

    added pages and services

--- phones/src/app/phone-details/phone-details.module.ts
+++ phones/src/app/phone-details/phone-details.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
 
 import { IonicModule } from '@ionic/angular';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { PhoneDetailsPage } from './phone-details.page';
 
@@ -18,9 +23,14 @@ const routes: Routes = [
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     IonicModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
     RouterModule.forChild(routes)
   ],
   declarations: [PhoneDetailsPage]
 })
-export class PhoneDetailsPageModule {}
+export class PhoneDetailsPageModule { }
