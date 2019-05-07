73d788a5d8a5d6f70e7f494def17fc12e4796daf
Piotr <piotr@piotr.tarasinski>
2019-05-07 09:58:03 +0200

    created project structure

---
+++ phones/src/app/phone-details/phone-details.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { PhoneDetailsPage } from './phone-details.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PhoneDetailsPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [PhoneDetailsPage]
+})
+export class PhoneDetailsPageModule {}
---
+++ phones/src/app/phone-details/phone-details.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-phone-details',
+  templateUrl: './phone-details.page.html',
+  styleUrls: ['./phone-details.page.scss'],
+})
+export class PhoneDetailsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
